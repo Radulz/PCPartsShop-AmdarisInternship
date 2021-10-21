ee1eb92a3e9de61d0bc078401afc52a518dc3975
Mircea-Radu Matei <mircea.matei@student.upt.ro>
2021-10-21 14:10:40 +0300

    Created the generic interface

---
+++ PCPartsShop/PCPartsShop/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public T Add(T item);
+        public T GetItem(int id);
+        public List<T> GetAllTs();
+        public T Delete(int id);
+        public T Update(int id);
+    }
+}
