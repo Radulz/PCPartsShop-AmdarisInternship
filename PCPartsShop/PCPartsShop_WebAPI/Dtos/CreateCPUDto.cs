d0aa3a42a59263c58a948b168b127ccd23165aa6
Mircea-Radu Matei <mircea.matei@student.upt.ro>
2021-12-06 17:02:43 +0200

    Set up automapper for cpu controller.

---
+++ PCPartsShop/PCPartsShop_WebAPI/Dtos/CreateCPUDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.Dtos
+{
+    public class CreateCPUDto
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public double Frequency { get; set; } 
+        public string Socket { get; set; }
+        public int Technology { get; set; } 
+        public int MemoryFrequency { get; set; } 
+        public int ThermalDesignPower { get; set; } 
+        public int NumberOfCores { get; set; }
+    }
+}
---
+++ PCPartsShop/PCPartsShop_WebAPI/Profiles/CPUProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using PCPartsShop.Application.Commands.CPUCommands.CreateCPU;
+using PCPartsShop.Dtos;
+using PCPartsShop_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.Profiles
+{
+    public class CPUProfile : Profile
+    {
+        public CPUProfile()
+        {
+            CreateMap<CPU, GetCPUDto>()
+                .ForMember(c => c.CPUId, opt => opt.MapFrom(c1 => c1.ComponentId))
+                .ForMember(c => c.Frequency, opt => opt.MapFrom(c1 => c1.Freq))
+                .ForMember(c => c.Technology, opt => opt.MapFrom(c1 => c1.Tech))
+                .ForMember(c => c.MemoryFrequency, opt => opt.MapFrom(c1 => c1.MFreq))
+                .ForMember(c => c.ThermalDesignPower, opt => opt.MapFrom(c1 => c1.TDP))
+                .ForMember(c => c.NumberOfCores, opt => opt.MapFrom(c1 => c1.Cores));
+
+            CreateMap<CreateCPUDto, CreateCPUCommand>()
+                .ForMember(c => c.Freq, opt => opt.MapFrom(c1 => c1.Frequency))
+                .ForMember(c => c.Tech, opt => opt.MapFrom(c1 => c1.Technology))
+                .ForMember(c => c.MFreq, opt => opt.MapFrom(c1 => c1.MemoryFrequency))
+                .ForMember(c => c.TDP, opt => opt.MapFrom(c1 => c1.ThermalDesignPower))
+                .ForMember(c => c.Cores, opt => opt.MapFrom(c1 => c1.NumberOfCores));
+        }
+    }
+}
