830856d77344b75eaea4bdc21a133c632ff92c3c
Mircea-Radu Matei <mircea.matei@student.upt.ro>
2021-12-22 12:30:57 +0200

    Added user and order models.

---
+++ PCPartsShop/PCPartsShop.Models/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using PCPartsShop_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.Domain.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string UserEmail { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserCounty { get; set; }
+        public string UserCity { get; set; }
+        public string UserAddress { get; set; }
+        public bool IsShipped { get; set; }
+
+        public ICollection<Component> OrderItems { get; set; }
+    }
+}
---
+++ PCPartsShop/PCPartsShop.Models/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.Domain.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string County { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public bool Admin { get; set; }
+
+        public User()
+        {
+            UserId = Guid.NewGuid();
+        }
+    }
+}
