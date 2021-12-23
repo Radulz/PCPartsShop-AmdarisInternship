cf24f41fb36907fd10a408cef2fe74c1fba780b7
Mircea-Radu Matei <mircea.matei@student.upt.ro>
2021-12-24 00:53:27 +0200

    Implemented order Dto and OrderProfile for automapper.

---
+++ PCPartsShop/PCPartsShop_WebAPI/Dtos/GetOrderDto.cs
@@ -0,0 +1,23 @@
+﻿using PCPartsShop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.WebAPI.Dtos
+{
+    public class GetOrderDto
+    {
+        public int OrderId { get; set; }
+        public string UserEmail { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserCounty { get; set; }
+        public string UserCity { get; set; }
+        public string UserAddress { get; set; }
+        public double TotalPrice { get; set; }
+        public bool ShippingStatus { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ PCPartsShop/PCPartsShop_WebAPI/Profiles/OrderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PCPartsShop.Domain.Models;
+using PCPartsShop.WebAPI.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartsShop.WebAPI.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, GetOrderDto>().ForMember(p => p.ShippingStatus, opt => opt.MapFrom(p1 => p1.IsShipped));
+        }
+    }
+}
