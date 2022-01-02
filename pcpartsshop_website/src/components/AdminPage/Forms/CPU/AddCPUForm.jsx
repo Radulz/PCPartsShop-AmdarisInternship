bd276c7657b7971b9a838e9beb93537490a2c387
Mircea-Radu Matei <mircea.matei@student.upt.ro>
2022-01-02 18:29:33 +0200

    Added jsx files for each form. Implemented the "Add CPU form".

---
+++ pcpartsshop_website/src/components/AdminPage/Forms/CPU/AddCPUForm.jsx
@@ -0,0 +1,264 @@
+import React from "react";
+import {
+  Button,
+  Grid,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
+} from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
+import * as constants from "../../../../constants/CPUConstants";
+import { joiResolver } from "@hookform/resolvers/joi";
+import Joi from "joi";
+
+const schema = Joi.object({
+  make: Joi.string().required(),
+  model: Joi.string().required(),
+  price: Joi.number().precision(2).required(),
+  image: Joi.string().required(),
+  frequency: Joi.number().precision(2).required(),
+  socket: Joi.string().required(),
+  technology: Joi.number().integer().required(),
+  memoryFrequency: Joi.number().integer().required(),
+  thermalDesignPower: Joi.number().integer().required(),
+  numberOfCores: Joi.number().integer().required(),
+});
+
+const AddCPUForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      make: "",
+      model: "",
+      price: "",
+      image: "",
+      frequency: "",
+      socket: "",
+      technology: "",
+      memoryFrequency: "",
+      thermalDesignPower: "",
+      numberOfCores: "",
+    },
+    resolver: joiResolver(schema),
+  });
+  console.log(errors);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.MAKE}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {" "}
+                  {constants.MAKE_LABEL}{" "}
+                </InputLabel>
+                <Input {...field} error={!!errors.make} />
+                {errors.make ? (
+                  <FormHelperText error>Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText disabled> </FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.MODEL}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {" "}
+                  {constants.MODEL_LABEL}{" "}
+                </InputLabel>
+                <Input {...field} error={!!errors.model} />
+                {errors.model ? (
+                  <FormHelperText error>Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText disabled> </FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.PRICE}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.PRICE_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.price} />
+                {errors.price ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>In dollars</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.IMAGE}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.IMAGE_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.image} />
+                {errors.image ? (
+                  <FormHelperText error>URL. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Link To Image</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.FREQUENCY}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.FREQUENCY_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.frequency} />
+                {errors.frequency ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Decimal Number (GHz)</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.SOCKET}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.SOCKET_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.socket} />
+                {errors.socket ? (
+                  <FormHelperText error>Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText disabled> </FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.TECHNOLOGY}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.TECHNOLOGY_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.technology} />
+                {errors.technology ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Integer number (nm)</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.MEMORYFREQUENCY}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.MEMORYFREQUENCY_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.memoryFrequency} />
+                {errors.memoryFrequency ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Integer Number (MHz)</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.THERMALDESGINPOWER}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.THERMALDESGINPOWER_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.thermalDesignPower} />
+                {errors.thermalDesignPower ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Integer number (W)</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Controller
+            name={constants.NUMBEROFCORES}
+            control={control}
+            render={({ field }) => (
+              <FormControl>
+                <InputLabel htmlFor="component-simple">
+                  {constants.NUMBEROFCORES_LABEL}
+                </InputLabel>
+                <Input {...field} error={!!errors.numberOfCores} />
+                {errors.numberOfCores ? (
+                  <FormHelperText error>Number. Field required.</FormHelperText>
+                ) : (
+                  <FormHelperText>Integer number</FormHelperText>
+                )}
+              </FormControl>
+            )}
+          />
+        </Grid>
+      </Grid>
+      <br />
+      <div
+        style={{
+          display: "flex",
+          marginTop: "25px",
+          justifyContent: "center",
+        }}
+      >
+        <Button variant="contained" type="submit" color="primary">
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+export default AddCPUForm;
